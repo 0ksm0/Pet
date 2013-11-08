5b7ce912cd35b5cae5b083e042751f1d369abec4
Stepan <0ksm0@ukr.net>
2013-11-08 19:05:43 +0200

    My first steps

--- src/ua/com/java/Fish.java
+++ src/ua/com/java/Fish.java
@@ -4,8 +4,13 @@ public class Fish extends Pet{
     int currentDepth = 0;
     public int dive(int howDepth){
         currentDepth = currentDepth + howDepth;
-        System.out.println("Ниряю на глубину " +howDepth +" метри");
-        System.out.println("Я на глубині " +currentDepth +" метри");
+        if (currentDepth >100){
+            System.out.println("Ти що офігів, сам ниряй на таку глибину, я боюсь");
+            currentDepth =currentDepth -howDepth;
+        } else{
+            System.out.println("Ниряю на глубину " +howDepth +" метри");
+            System.out.println("Я на глубині " +currentDepth +" метри");
+        }
         return currentDepth;
     }
 }
--- src/ua/com/java/FishMaster.java
+++ src/ua/com/java/FishMaster.java
@@ -3,8 +3,9 @@ package ua.com.java;
 public class FishMaster {
     public static void main(String[] args) {
         Fish myFish = new Fish();
-        myFish.dive(2);
+        myFish.dive(93);
         myFish.dive(4);
+        myFish.dive(5);
         myFish.sleep();
     }
 }
